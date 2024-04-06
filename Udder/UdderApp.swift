2d4435901569c3134ff65a21105a0b2bfc8f0691
Muradmmyz <muradhasanov851@gmail.com>
2024-04-06 23:37:20 +0300

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Murad Hasanov on 6.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
